659572b6283c5e3efc0435149af94acff45827cd
Alex Strelets <alex.strelets@gmail.com>
2025-01-06 21:46:52 +0200

    Update dsssp dependency version and enhance Vite configuration for production builds

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/',
-  server: {
-    port: 3030,
-    open: true
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'development' ? '/' : '/sound/dsssp/',
+    server: {
+      port: 3030,
+      open: true
+    },
+    build: {
+      sourcemap: true,
+      rollupOptions: {}
+    }
   }
 })
