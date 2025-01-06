43ff59d53552c6aaa9b5bce300563fbfe51eb78e
Alex Strelets <alex.strelets@gmail.com>
2025-01-06 22:31:04 +0200

    Add GitHub Actions workflow for demo deployment and update Vite config for new base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
-    base: mode === 'development' ? '/' : '/sound/dsssp/',
+    base: mode === 'development' ? '/' : '/dsssp-demo/',
+    // base: '/',
     server: {
       port: 3030,
       open: true
