1758ee8eb7cd403fa8077676793371e56d218184
Alex Strelets <alex.strelets@gmail.com>
2025-01-20 13:39:55 +0200

    feat: add build configurations for GitHub and Landing

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,23 @@ import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import svgr from 'vite-plugin-svgr'
 
+const getBase = (mode: string) => {
+  switch (mode) {
+    case 'github':
+      return '/dsssp-demo/'
+    case 'landing':
+      return '/demo'
+    case 'development':
+    default:
+      return '/'
+  }
+}
+
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   return {
     plugins: [svgr(), react()],
-    base: mode === 'development' ? '/' : '/dsssp-demo/',
-    // base: '/',
+    base: getBase(mode),
     server: {
       port: 3003,
       open: true
