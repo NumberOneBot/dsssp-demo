ce2e077e47ef981c27be91af2f647f419c7ad6c0
Alex Strelets <alex.strelets@gmail.com>
2025-02-02 17:51:30 +0200

    feat: update NavBar styling

--- src/pages/components/NavBar.tsx
+++ src/pages/components/NavBar.tsx
@@ -1,27 +1,27 @@
 const NavBar = () => {
   return (
-    <nav className="mt-4 space-x-4">
+    <nav className="mt-4 space-x-4 font-semibold text-lg">
       <a
         href="#/"
-        className="text-blue-500 hover:text-blue-400"
+        className="text-white hover:text-blue-400"
       >
         Main Project
       </a>
       <a
         href="#demo2"
-        className="text-blue-500 hover:text-blue-400"
+        className="text-white hover:text-blue-400"
       >
         Demo 2
       </a>
       <a
         href="#demo3"
-        className="text-blue-500 hover:text-blue-400"
+        className="text-white hover:text-blue-400"
       >
         Demo 3
       </a>
       <a
         href="#demo4"
-        className="text-blue-500 hover:text-blue-400"
+        className="text-white hover:text-blue-400"
       >
         Demo 4
       </a>
